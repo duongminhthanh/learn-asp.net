a31662e2547cfa5cd17458a0e5e8fc999ef232ab
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-01-28 16:12:17 +0700

    define data model

---
+++ GAMESTORE/GameStore.Api/GameStore.Api/Entities/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.Api.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        // required name
+        public required string Name { get; set; }
+    }
+}
--- GAMESTORE/GameStore.Api/GameStore.Api/Program.cs
+++ GAMESTORE/GameStore.Api/GameStore.Api/Program.cs
@@ -1,7 +1,13 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Define a connection string
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGameEndpoints();
