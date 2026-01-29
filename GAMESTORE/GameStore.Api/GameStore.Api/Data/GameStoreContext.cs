89d7dec4bbde4cf424a723b95ba40da738865426
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-01-29 15:58:17 +0700

    seeding data

--- GAMESTORE/GameStore.Api/GameStore.Api/Data/GameStoreContext.cs
+++ GAMESTORE/GameStore.Api/GameStore.Api/Data/GameStoreContext.cs
@@ -8,5 +8,20 @@ namespace GameStore.Api.Data
     {
         public DbSet<Game> Games => Set<Game>();
         public DbSet<Genre> Genres => Set<Genre>();
+
+        //Seeding data
+        // This method is called when the database model is being created
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure default seed data for the 'Genre' entity
+            modelBuilder.Entity<Genre>().HasData(
+                // Each 'new' object represents a row in the Genres table
+                new { Id = 1, Name = "Fighting" },
+                new { Id = 2, Name = "Roleplaying" },
+                new { Id = 3, Name = "Sports" },
+                new { Id = 4, Name = "Racing" },
+                new { Id = 5, Name = "Kids and Family" }
+            );
+        }
     }
 }
