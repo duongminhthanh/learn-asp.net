97d44c87c682a3390ed42c7a7b64420ae94bcb20
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-01-30 10:47:52 +0700

    register GameStoreContext in Dependency Injection container as Scoped service

--- GAMESTORE/GameStore.Api/GameStore.Api/Program.cs
+++ GAMESTORE/GameStore.Api/GameStore.Api/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Define a connection string
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
-
+// Registers GameStoreContext with a 'Scoped' lifetime in the Dependency Injection container.
+// 'Scoped' means a new instance of GameStoreContext is created once per client request (HTTP request).
+// It ensures that the same database connection is reused throughout a single request's lifecycle.
+builder.Services.AddScoped<GameStoreContext>();
 var app = builder.Build();
 
 app.MapGameEndpoints();
