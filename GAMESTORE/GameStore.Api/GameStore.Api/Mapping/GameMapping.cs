0161e921ba753c4bf6098a63dc5e525c23c4b72e
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-01-30 16:30:59 +0700

    add extesion map to entity and map to dto

---
+++ GAMESTORE/GameStore.Api/GameStore.Api/Mapping/GameMapping.cs
@@ -0,0 +1,33 @@
+﻿using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Mapping
+{
+    public static class GameMapping
+    {
+        public static Game ToEntity(this CreateGameDto game)
+        {
+            return new Game()
+            {
+                Name = game.Name,
+                GenreId = game.GenreId,
+                Price = game.Price,
+                ReleaseDate = game.ReleaseDate
+
+            };
+
+        }
+
+        public static GameDto ToDto(this Game game)
+        {
+            return new
+            (
+                 game.Id,
+                 game.Name,
+                 game.Genre!.Name, // Use '!' because we expect the genre to exist after seeding
+                 game.Price,
+                 game.ReleaseDate
+            );
+        }
+    }
+}
